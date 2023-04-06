5c9a51f7fa85475673b6f84b90f984e9e3d4845e
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-04-06 14:56:38 -0500

    adding Java lessons and exercises

---
+++ src/ConsoleIOLecture.java
@@ -0,0 +1,6 @@
+public class ConsoleIOLecture {
+    public static void main(String[] args) {
+        String cohort = "Bigfoot";
+        System.out.printf("Hello, %s!", cohort);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        int myFavoriteNumber = 17;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string.";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int y = 5;
+//        System.out.println(y++);
+//        System.out.println(y);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//
+        int x = 10;
+        int y = 2;
+        x /= y;
+        System.out.println(x);
+        y -= x;
+        System.out.println(y);
+
+    }
+}
