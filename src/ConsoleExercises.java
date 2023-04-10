95a9bee42ad3e61d788941064e71181bc73fab52
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-04-10 12:33:31 -0500

    Adding Console Exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.%n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter an integer:");
+//        Integer userInteger = scanner.nextInt();
+//        System.out.println("You entered " + userInteger);
+//
+//        System.out.println("Enter 1st word:");
+//        String firstWord = scanner.next();
+//        System.out.println("Enter 2nd word:");
+//        String secondWord = scanner.next();
+//        System.out.println("Enter 3rd word:");
+//        String thirdWord = scanner.next();
+//        System.out.printf("Your words are:%n %s,%n %s,%n %s%n", firstWord, secondWord, thirdWord);
+//
+//        System.out.println("Please write a sentence:");
+//        String nextLine = scanner.nextLine();
+//        String userSentence = scanner.nextLine();
+//        System.out.println("Your sentence is: " + userSentence);
+
+        System.out.println("What is the length of the classroom:");
+        Double lengthClass = scanner.nextDouble();
+        System.out.println("What is the width of the classroom:");
+        Double widthClass = scanner.nextDouble();
+        System.out.println("What is the height of the classroom:");
+        Double heightClass = scanner.nextDouble();
+//        System.out.println("The length and width of the classroom are: " + Integer.parseInt(lengthClass) + " by " + Integer.parseInt(widthClass));
+//        System.out.println("The area of the room is: " + (Integer.parseInt(lengthClass)*Integer.parseInt(widthClass)));
+//        System.out.println("The perimeter of the room is: " + ((Integer.parseInt(lengthClass)*2)+(Integer.parseInt(widthClass)*2)));
+//        scanner.useDelimiter("\n");
+//        System.out.println("The volume of the room is " + (Integer.parseInt(lengthClass)*Integer.parseInt(widthClass)*Integer.parseInt(heightClass)));
+        System.out.println("The length and width of the classroom are: " + lengthClass + " by " + widthClass);
+        System.out.println("The area of the room is: " + (lengthClass*widthClass));
+        System.out.println("The perimeter of the room is: " + ((lengthClass*2)+(widthClass*2)));
+        scanner.useDelimiter("\n");
+        Double volumeClass = (lengthClass*widthClass*heightClass);
+        System.out.printf("The volume of the room is: %s", volumeClass);
+
+
+    }
+}
