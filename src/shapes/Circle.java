d85c1ffde53700772250593cb9d02ecd9cb93fa1
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-04-13 15:14:39 -0500

    final changes to the circles exercises

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,13 +1,27 @@
 package shapes;
 
+import util.Input;
+
 public class Circle {
+
+    public static int totalCircles;
+
     //Declare private radius field
     private double radius;
 
     // Constructor for Circle class
     // Sets the value of radius when an instance of this class is created
     public Circle(double radius) {
+
         this.radius = radius;
+        totalCircles++;
+    }
+
+    public static void outputCircleInfo(){
+        double radius = new Input().getDouble("Please enter the radius of the circle:");
+        Circle circle = new Circle(radius);
+        System.out.printf("The area of your circle is: %.2f%n", circle.getArea());
+        System.out.printf("The circumference of your circle is: %.2f%n", circle.getCircumference());
     }
 //    public double getRadius(){
 //        return radius;
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -3,8 +3,18 @@ package shapes;
 import util.Input;
 public class CircleApp {
     public static void main(String[] args) {
+
+        Input input = new Input();
+        String userChoice = "y";
+        while (input.yesNo(userChoice)) {
+            Circle.outputCircleInfo();
+            userChoice = input.getString("Would you like to create another circle?");
+        }
+        System.out.println(Circle.totalCircles);
+
+
         //this is only ok if you only have to instantiate an input object ONCE
-        double radius = new Input().getDouble("Please enter the radius of the circle:");
+//        double radius = new Input().getDouble("Please enter the radius of the circle:");
 
 
         //TODO: Creates an instance of Input class to get user input: THIS GOES FIRST IF YOU NEED MORE THAN ONE INSTANCE
@@ -15,10 +25,12 @@ public class CircleApp {
 
 
         // Create a Circle object with the given radius
-        Circle circle = new Circle(radius);
+//        Circle circle = new Circle(radius);
 
         // Display the circumference and area of the circle
-        System.out.printf("Circumference: %.2f%n", circle.getCircumference());
-        System.out.printf("Area: %.2f%n", circle.getArea());
+  //      System.out.printf("Circumference: %.2f%n", circle.getCircumference());
+    //    System.out.printf("Area: %.2f%n", circle.getArea());
+
+
     }
 }
